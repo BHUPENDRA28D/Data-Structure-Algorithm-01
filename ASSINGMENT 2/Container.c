a8693b116c45ab45c161ed6ecdc41388e3415836
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2022-02-02 00:21:06 +0530

    Added Append and Insert Function

--- ASSINGMENT 2/Container.c
+++ ASSINGMENT 2/Container.c
@@ -15,13 +15,31 @@ struct ArrayADT* creatArray(int cap)
     arr->ptr = (int *)malloc(sizeof(int) * cap);
     return arr;
 }
+void append(struct ArrayADT *arr, int data){
+    if(arr->lastIndex+1==arr->capacity){
+        printf("overflow\n");
+    }
+    else{
+        (arr->lastIndex+=1);
+        arr->ptr[arr->lastIndex]= data;
+     }//int i;
+    // for(i=0;i<arr->capacity;i++){
+    printf("%d\n",arr->ptr[0]);
+}
+void insert(struct ArrayADT *arr,int data){
+    int pos =3;
+    
+    for(int i=arr->capacity;i>arr->ptr[pos];i--){
+            arr->ptr[i]=arr->ptr[i-1];
+    }
+    arr->ptr[pos-1]=data;
+    printf("%d\n",arr->ptr[2]);
+}
 int main()
-{
-
-    int i, result;
-    printf("Enter the capacity of array\n");
-    scanf("%d", &i);
-    result = creatArray(i);
-
+{   struct ArrayADT *arr;
+    arr = creatArray(4);
+    
+    append(arr,20);
+    insert(arr,5);
     return 0;
 }
