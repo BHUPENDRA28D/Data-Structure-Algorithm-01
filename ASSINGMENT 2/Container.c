e9ac49bbe21afa7b2845118f81ba39372d0a6499
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2022-02-02 13:50:51 +0530

    Added delet and count functions

--- ASSINGMENT 2/Container.c
+++ ASSINGMENT 2/Container.c
@@ -6,7 +6,8 @@ struct ArrayADT
     int lastIndex;
     int *ptr;
 };
-struct ArrayADT* creatArray(int cap)
+// Created a Array here (dynamicaly)
+struct ArrayADT *creatArray(int cap)
 {
     struct ArrayADT *arr = NULL;
     arr = (struct ArrayADT *)malloc(sizeof(struct ArrayADT));
@@ -15,31 +16,59 @@ struct ArrayADT* creatArray(int cap)
     arr->ptr = (int *)malloc(sizeof(int) * cap);
     return arr;
 }
-void append(struct ArrayADT *arr, int data){
-    if(arr->lastIndex+1==arr->capacity){
+// Append function (Enter value after last index)
+void append(struct ArrayADT *arr, int data)
+{
+    if (arr->lastIndex + 1 == arr->capacity)
+    {
         printf("overflow\n");
     }
-    else{
-        (arr->lastIndex+=1);
-        arr->ptr[arr->lastIndex]= data;
-     }//int i;
+    else
+    {
+        (arr->lastIndex += 1);
+        arr->ptr[arr->lastIndex] = data;
+    } // int i;
     // for(i=0;i<arr->capacity;i++){
-    printf("%d\n",arr->ptr[0]);
+    printf("%d\n", arr->ptr[0]);
 }
-void insert(struct ArrayADT *arr,int data){
-    int pos =3;
-    
-    for(int i=arr->capacity;i>arr->ptr[pos];i--){
-            arr->ptr[i]=arr->ptr[i-1];
+// Insert function (insert value at given position)
+void insert(struct ArrayADT *arr, int data, int position)
+{
+
+    for (int i = arr->capacity; i > position; i--)
+    {
+        arr->ptr[i] = arr->ptr[i - 1];
     }
-    arr->ptr[pos-1]=data;
-    printf("%d\n",arr->ptr[2]);
+    arr->ptr[position - 1] = data;
+    printf("%d\n", arr->ptr[2]);
+}
+// Delet value at given posotion
+void delet(struct ArrayADT *arr, int position)
+{
+    if (position > arr->capacity)
+    {
+        printf("Deletion not possible\n");
+    }
+    else
+        for (int i = position - 1; i < arr->capacity; i++)
+        {
+            arr->ptr[i] = arr->ptr[i + 1];
+        }
+    printf("%d\n", arr->ptr[2]);
+} // count function
+int count(struct ArrayADT *arr)
+{
+    return sizeof(arr->ptr) / sizeof(arr->ptr[0]);
 }
 int main()
-{   struct ArrayADT *arr;
+{
+    struct ArrayADT *arr;
     arr = creatArray(4);
-    
-    append(arr,20);
-    insert(arr,5);
+
+    append(arr, 20);
+    insert(arr, 5, 3);
+
+    delet(arr, 3);
+    printf("%d\n", count(arr));
     return 0;
 }
