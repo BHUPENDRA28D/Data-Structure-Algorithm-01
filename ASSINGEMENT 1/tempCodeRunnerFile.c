a1ee1dda8f8596d89c0f6ac3de02b21e33c4ff96
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2022-02-20 17:20:25 +0530

    Added insert ,delet,count,veiw,edit,getline,search functions InThe abstract Data type

--- ASSINGEMENT 1/tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-s_avg =minAvg(day,3);
-    // printf("Minimun avg of several day is %f\n",s_avg);
--- ASSINGMENT 2/Container.c
+++ ASSINGMENT 2/Container.c
@@ -21,7 +21,7 @@ void append(struct ArrayADT *arr, int data)
 {
     if (arr->lastIndex + 1 == arr->capacity)
     {
-        printf("overflow\n");
+        printf("\noverflow\n");
     }
     else
     {
@@ -31,44 +31,93 @@ void append(struct ArrayADT *arr, int data)
     // for(i=0;i<arr->capacity;i++){
     printf("%d\n", arr->ptr[0]);
 }
-// Insert function (insert value at given position)
-void insert(struct ArrayADT *arr, int data, int position)
-{
 
-    for (int i = arr->capacity; i > position; i--)
-    {
-        arr->ptr[i] = arr->ptr[i - 1];
+// Insert function (insert value at given position)
+void insert(struct ArrayADT *arr, int data, int index)
+{   int i;
+    if(index<0 || index>arr->lastIndex+1){
+        printf("\nInsertion Error :: Invalid Index\n");    
     }
-    arr->ptr[position - 1] = data;
-    printf("%d\n", arr->ptr[2]);
-}
+    else if(index==arr->capacity-1)
+        printf("\nInsertion Error:: Overflow\n");
+        else{
+            for(i=arr->lastIndex;i>=index;i++)
+                arr->ptr[i+1]=arr->ptr[i];
+
+            arr->ptr[i]= data;
+        
+    arr->lastIndex++;
+}}
+
 // Delet value at given posotion
-void delet(struct ArrayADT *arr, int position)
+void delet(struct ArrayADT *arr, int index)
 {
-    if (position > arr->capacity)
+    if (index<0 || index>arr->lastIndex)
     {
-        printf("Deletion not possible\n");
+        printf("\nInvalid Index::Deletion not possible\n");
     }
     else
-        for (int i = position - 1; i < arr->capacity; i++)
+        for (int i = index; i < arr->lastIndex; i++)
         {
             arr->ptr[i] = arr->ptr[i + 1];
-        }
+        
+        arr->lastIndex--;}
     printf("%d\n", arr->ptr[2]);
-} // count function
+} 
+
+// count function
 int count(struct ArrayADT *arr)
 {
-    return sizeof(arr->ptr) / sizeof(arr->ptr[0]);
+    return arr->lastIndex+1;
 }
+// view the array
+void view(struct ArrayADT *arr){
+    int i;
+    for(i=0;i<=arr->lastIndex;i++)
+       printf("%d",arr->ptr[i]);
+}
+// edit function to edit a number at given index
+void edit(struct ArrayADT *arr,int NewData, int index){
+    if(index>arr->lastIndex || index<0)
+        printf("\nEdition Error:: Invalid Index");
+
+    else
+        arr->ptr[index]=NewData;   
+}
+
+// getting an item on specific index
+void getItem(struct ArrayADT *arr,int index){
+    if(index>arr->lastIndex || index<0)
+        printf("\nError:: Invalid Index");
+    else
+        printf("\n%d",arr->ptr[index]);
+            }
+// search function tells us the data given is on the index or not
+int search(struct ArrayADT*arr,int data){
+    printf("\n");
+    for(int i=0;i<arr->lastIndex;i++){
+        if(arr->ptr[i]== data)
+           return i;
+    }
+    return -1;
+}
+
 int main()
 {
     struct ArrayADT *arr;
     arr = creatArray(4);
 
     append(arr, 20);
-    insert(arr, 5, 3);
+    insert(arr,20, 0);
 
-    delet(arr, 3);
-    printf("%d\n", count(arr));
+    delet(arr, 0);
+
+    printf("\n%d\n", count(arr));
+
+    view(arr);
+    edit(arr,25,0);
+    getItem(arr,0);
+    int x =search(arr,25);
+    printf("\n Search success :: %d",x);
     return 0;
 }
